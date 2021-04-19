ca8497f58459062783d5581b165d56df513a8995
Brian Westphal <coding@brianwestphal.com>
2021-04-18 20:24:22 -0700

    Create save_model.py
    Create test h5 saver

---
+++ save_model.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+import os
+import argparse
+import json
+import pickle
+import dnnlib
+from dnnlib.tflib as tflib
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--network', help='Network pickle filename', dest='network', required=True)
+    parser.add_argument('--destination', help='Output h5 filename', dest='destination', required=True)
+    
+    args = parser.parse_args()
+    
+    with open(args.network, 'rb') as f:
+        _, _, Gs = pickle.load(f)
+
+    tf.keras.models.save_model(
+        Gs,
+        args.destination,
+        overwrite=True,
+        include_optimizer=True,
+        save_format=None,
+        signatures=None
+    )
